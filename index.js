fad831e5c6a8d8e91fd70fb0d012a2be85a605d6
Rohit Sharma <rohit3171999@gmail.com>
2026-06-03 18:24:54 +0530

    Add items endpoint to return item data

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ const students = [
     {id: 4, name: "dhoni"}   
 ];
 
+const items = [
+    {id:1, name: "laptop"},
+    {id:2, name: "mobile"},
+    {id:3, name: "tablet"},
+    {id:4, name: "monitor"}
+]
+
 app.get("/students", (req, res) => {
     res.json(students);
+});
+
+app.get("/items", (req, res) => {
+    res.json(items);
 });
 
 app.listen(3000, () => {
