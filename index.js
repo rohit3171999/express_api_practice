d5b97bcb4bbc4f6f2c1ec540756d6179dbf312c3
Rohit Sharma <rohit3171999@gmail.com>
2026-06-03 17:56:41 +0530

    Comment out old API message for clarity

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-console.log("API's are coming Soon!!")
+//console.log("API's are coming Soon!!")
 console.log("API's are coming Soon testing again!!")
