b40e53c5eac6f8ff6bc51ecfe82d758f7709726c
Rohit Sharma <rohit3171999@gmail.com>
2026-06-03 18:14:12 +0530

    Add students endpoint to return student data

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get("/", (req, res) => {
     });
 });
 
+const students = [
+    {id: 1, name: "rohit"},
+    {id: 2, name: "sachin"},
+    {id: 3, name: "virat"},
+    {id: 4, name: "dhoni"}   
+];
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
